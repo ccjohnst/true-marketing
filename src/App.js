a5d5f9858f9bba1d742a2f6c3c67f9c56a72292d
Chris Johnston <christopherjohnston@protonmail.com>
2021-08-13 17:38:57 +0100

    switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Services from './components/pages/Services';
 import Contact from './components/pages/Contact';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import NavigationBar from './components/navigation';
 import Footer from './components/footer';
 
@@ -15,14 +15,10 @@ function App() {
         <ChakraProvider theme={theme}>
           <NavigationBar />
           <Switch>
-            <Route path="/" exact component={() => <Home />}></Route>
-            <Route path="/about" exact component={() => <About />}></Route>
-            <Route
-              path="/services"
-              exact
-              component={() => <Services />}
-            ></Route>
-            <Route path="/contact" exact component={() => <Contact />}></Route>
+            <Route path="/" exact component={() => <Home />} />
+            <Route path="/about" exact component={() => <About />} />
+            <Route path="/services" exact component={() => <Services />} />
+            <Route path="/contact" exact component={() => <Contact />} />
           </Switch>
           <Footer />
         </ChakraProvider>
